69afc0318d4b8a75d780228313a59b656e2bf40a
Educrispim <educrispim@edu.unifil.br>
2026-04-15 15:30:41 -0300

    Alteracao dos patterns

--- server.js
+++ server.js
@@ -38,7 +38,12 @@ const server = https.createServer(options, (req, res) => {
                 res.end('Erro no servidor: ' + error.code);
             }
         } else {
-            res.writeHead(200, { 'Content-Type': contentType });
+            res.writeHead(200, {
+                'Content-Type': contentType,
+                'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                'Pragma': 'no-cache',
+                'Expires': '0'
+            });
             res.end(content, 'utf-8');
         }
     });
